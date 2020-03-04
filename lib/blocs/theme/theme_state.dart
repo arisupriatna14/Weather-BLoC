87e0e384fa413d64b49c5dc861fcb95ac8207493
arisupriatna14 <arisupriatna703@gmail.com>
2020-03-04 11:37:22 +0700

    fix identation

--- lib/blocs/theme/theme_state.dart
+++ lib/blocs/theme/theme_state.dart
@@ -2,14 +2,13 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
 class ThemeState extends Equatable {
-	final ThemeData theme;
-	final MaterialColor color;
+  final ThemeData theme;
+  final MaterialColor color;
 
-	const ThemeState({@required this.theme, @required this.color})
-			: assert(theme != null),
-				assert(color != null);
+  const ThemeState({@required this.theme, @required this.color})
+      : assert(theme != null),
+        assert(color != null);
 
-	@override
-	// TODO: implement props
-	List<Object> get props => [theme, color];
+  @override
+  List<Object> get props => [theme, color];
 }
